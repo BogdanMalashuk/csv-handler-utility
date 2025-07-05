038257e957883f4ab30e19c91c91c33e8a1725b3
BogdanMalashuk <kufar.120@gmail.com>
2025-07-05 10:19:56 +0300

    compare symbol '==' in input string fixed to '='

--- utils.py
+++ utils.py
@@ -2,8 +2,12 @@ import re
 
 
 def parse_condition(condition):
-    match = re.match(r'(\w+)\s*(==|>|<)\s*(.+)', condition)
+    match = re.match(r'(\w+)\s*(=|>|<)\s*(.+)', condition)
     if not match:
         raise ValueError("Invalid condition format")
     col, operator, val = match.groups()
+
+    if operator == '=':
+        operator = '=='
+
     return col, operator, val
