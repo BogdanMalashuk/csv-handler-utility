1ba0238b3bec9de84682269e9884e95a0330598d
BogdanMalashuk <kufar.120@gmail.com>
2025-07-05 10:02:45 +0300

    input string dividing (define compare condition)

---
+++ utils.py
@@ -0,0 +1,9 @@
+import re
+
+
+def parse_condition(condition):
+    match = re.match(r'(\w+)\s*(==|>|<)\s*(.+)', condition)
+    if not match:
+        raise ValueError("Invalid condition format")
+    col, operator, val = match.groups()
+    return col, operator, val
