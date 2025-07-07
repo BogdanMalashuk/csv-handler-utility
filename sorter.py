4ce87519fc50eb8608ef5a5e8d3dd1e6c8a6b3c6
BogdanMalashuk <kufar.120@gmail.com>
2025-07-07 10:32:54 +0300

    PEP8, annotations

--- sorter.py
+++ sorter.py
@@ -1,4 +1,7 @@
-def sort_data(data, column, direction):
+from typing import List, Dict
+
+
+def sort_data(data, column, direction) -> List[Dict[str, str]]:
     reverse = direction.lower() == 'desc'
     try:
         return sorted(data, key=lambda x: float(x[column]), reverse=reverse)
