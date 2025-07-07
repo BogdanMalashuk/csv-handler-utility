a895953dc9c31e5683cfe8056e05990b5fa63d88
BogdanMalashuk <kufar.120@gmail.com>
2025-07-07 10:31:33 +0300

    to PEP8 converting, annotations

--- aggregator.py
+++ aggregator.py
@@ -1,3 +1,7 @@
+from typing import List, Dict
+
+# Добавление новых функций для агрегации через словарь
+
 AGGREGATIONS = {
     'avg': lambda vals: round(sum(vals) / len(vals), 2),
     'min': min,
@@ -5,7 +9,7 @@ AGGREGATIONS = {
 }
 
 
-def aggregate(data, column, operator):
+def aggregate(data: List[Dict[str, str]], column: str, operator: str):
     values = [float(row[column]) for row in data]
     if operator not in AGGREGATIONS:
         raise ValueError(f"Unsupported aggregation: {operator}")
