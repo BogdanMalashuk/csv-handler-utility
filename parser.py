c24a975dd0ee23d50b1bc6ce99ddf41835ee846b
BogdanMalashuk <kufar.120@gmail.com>
2025-07-07 10:32:41 +0300

    custom errors

--- parser.py
+++ parser.py
@@ -1,8 +1,16 @@
 import argparse
+from argparse import Namespace
+import sys
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Process a CSV file.")
+class CustomArgumentParser(argparse.ArgumentParser):  # кастомные ошибки в понятном формате
+    def error(self, message):
+        print(f"[Ошибка аргумента] {message}")
+        sys.exit(2)
+
+
+def parse_args() -> Namespace:
+    parser = CustomArgumentParser(description="CSV Handler Utility")
     parser.add_argument('--file', required=True, help='Path to CSV file')
     parser.add_argument('--where', help='Filter condition, e.g., "price>100"')
     parser.add_argument('--aggregate', help='Aggregate column, e.g., "rating=avg"')
